ef990c097a8746f276f6b94b33788a8b95702b05
guilherme <gguilhermepires@gmail.com>
2021-10-12 13:17:02 -0300

    refatoração

--- lambda_function.py
+++ lambda_function.py
@@ -3,15 +3,11 @@ import pymysql
 import json
 
 def lambda_handler(event, context):
-    print('event: {}'.format(event))
-    print(context)
- 
-
     tabela = 'Usuario'
-    connection = pymysql.connect(host='desafio2.ctu9myaz68j0.us-east-1.rds.amazonaws.com',
-                             user='admin',
-                             password='augusto31',
-                             database='desafio',
+    connection = pymysql.connect(host='',
+                             user='',
+                             password='',
+                             database='',
                              cursorclass=pymysql.cursors.DictCursor)
     with connection.cursor() as cursor:
         acao = ''
@@ -23,10 +19,8 @@ def lambda_handler(event, context):
                 payload =  json.loads(event['body'])
                 acao = payload['acao']
             except Exception as ex:
-                print('erro converte')
                 print(ex)
 
-        print('acao: {}'.format(acao))
         if acao == 'inserir':
             sql = "INSERT INTO {} (nome, idade,cargo) VALUES ('{}', {}, '{}')".format(
                 tabela,
@@ -43,7 +37,6 @@ def lambda_handler(event, context):
             sql = "SELECT * FROM {} WHERE id={} ;".format(tabela, payload['id'])
             cursor.execute(sql)
             result = cursor.fetchone()
-            print(result)
             connection.close()
             return {'statusCode': 200,'body': json.dumps(result)}
 
@@ -61,7 +54,6 @@ def lambda_handler(event, context):
             return {'statusCode': 200,'body': json.dumps('atualizado com sucesso')}
 
         if acao == "buscaTodos":
-            print('buscado todos')
             cursor.execute('SELECT * from {}'.format(tabela))
             rows = cursor.fetchall()
             for row in rows:
